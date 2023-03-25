89470cb9edbec31cc39c8476afd1c4a1c356165d
Nikita\Nick <kineeetik@gmail.com>
2023-03-25 19:40:44 +0300

    Update

--- test/AbilitiesImplementations/LinearMove/LinearMoveAbility.gd
+++ test/AbilitiesImplementations/LinearMove/LinearMoveAbility.gd
@@ -5,9 +5,14 @@ class_name LinearMoveAbility
 const NAME = "LinearMoveAbility"
 
 func get_action(fo:FieldObject, apply_position:Vector2i):
-	var anim_action : Action = fo.visual_node.get_animate_ability_action(NAME, apply_position)
+	var abil_data : LinearMoveAbilityData = fo.abilities_data[LinearMoveAbility.NAME]
+	
+	if HexPlane.get_diatance(fo.position, apply_position) > abil_data.move_remains:
+		return EmptyAction.new()
 	
 	var res : MoveAction = MoveAction.new(fo, apply_position)
+	
+	var anim_action : Action = fo.visual_node.get_animate_ability_action(NAME, apply_position)
 	
 	if anim_action != null:
 		res.add_parallel_action(anim_action)
---
+++ test/GeneralActions/EmptyAction.gd
@@ -0,0 +1,6 @@
+extends Action
+
+class_name EmptyAction
+
+func _on_start():
+	completed = true
