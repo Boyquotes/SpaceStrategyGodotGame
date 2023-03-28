4bb6592d4da6b7705ff37264faca44bcb47611ea
Nikita\Nick <kineeetik@gmail.com>
2023-03-28 15:36:47 +0300

    Deducting test action

--- TestContent/AbilitiesImplementations/LinearMove/LinearMoveAbility.gd
+++ TestContent/AbilitiesImplementations/LinearMove/LinearMoveAbility.gd
@@ -4,10 +4,34 @@ class_name LinearMoveAbility
 
 const NAME = "LinearMoveAbility"
 
+
+class DeductMoveRemains:
+	extends Action
+	
+	var _ability_data : LinearMoveAbilityData
+	var _deduct_value : int
+	
+	func _init(data:LinearMoveAbilityData, how_many:int):
+		_ability_data = data
+		_deduct_value = how_many
+	
+	
+	func _on_start():
+		_ability_data.move_remains -= _deduct_value
+		completed = true
+	
+	
+	func _to_string():
+		return "DeductMoveRemains action: %s"%_deduct_value
+
+
+
 func _get_action(fo:FieldObject, apply_position:Vector2i) -> Action:
 	var abil_data : LinearMoveAbilityData = fo.abilities_data[NAME]
 	
-	if HexPlane.get_diatance(fo.position, apply_position) > abil_data.move_remains:
+	var move_distance : int = HexPlane.get_diatance(fo.position, apply_position)
+	
+	if move_distance > abil_data.move_remains:
 		return EmptyAction.new()
 	
 	var res : MoveAction = MoveAction.new(fo, apply_position)
@@ -18,5 +42,9 @@ func _get_action(fo:FieldObject, apply_position:Vector2i) -> Action:
 	
 	if anim_action != null:
 		res.add_parallel_action(anim_action)
+	
+	res.add_parallel_action(
+		DeductMoveRemains.new(abil_data, move_distance)
+	)
 	
 	return res
