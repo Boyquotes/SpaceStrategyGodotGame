86b6d655743aa6efc81c4558c06ba1e2645e9e09
Nikita\Nick <kineeetik@gmail.com>
2023-03-28 12:38:02 +0300

    Added two draw modes on ability draw rule

--- GlobalClasses/FieldVisualClasses/AbilityDrawRule.gd
+++ GlobalClasses/FieldVisualClasses/AbilityDrawRule.gd
@@ -3,6 +3,24 @@ extends FieldDrawRule
 class_name AbilityDrawRule
 
 
+var applying : bool = false
+
+
+func draw_applying():
+	applying = true
+
+
+func draw_waiting():
+	applying = false
+
+
+func _on_draw(field:GameField):
+	if applying:
+		_draw_on_applying(field)
+	else:
+		_draw_on_waiting(field)
+
+
 func start():
 	_on_start()
 
@@ -14,6 +32,13 @@ func update():
 func get_ability_name():
 	return _name()
 
+
+func _draw_on_applying(field:GameField):
+	pass
+
+
+func _draw_on_waiting(field:GameField):
+	pass
 
 # virtual
 func _on_update():
